4cb848ae0020e6c80e7e65d65094d951beb68824
Dev Jana <devjana@Devs-MacBook-Pro.local>
2019-04-17 15:43:22 -0500

    end of lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,10 +8,36 @@ function getMessages(){
     }).then( function( response ){
         // console log the response
         console.log( 'back from GET with:', response );
+        let el = $( '#messagesOut' );
+        el.empty();
+        // loop through response
+        for( let i =0; i< response.length; i++){
+            // display each on page
+            el.append( `<li>${ response[i].from} says: ${ response[i].text }</li>`)
+        }
     });
 }
 
+function newMessage(){
+    // get user input
+    // create an object
+    let objectToSend = {
+        from: $( '#fromIn' ).val(),
+        text: $( '#textIn' ).val()
+    }
+    // send object to server via POST
+    console.log( 'sending:', objectToSend );
+    $.ajax({
+        type: 'POST',
+        url: '/messages',
+        data: objectToSend
+    }).then( function( response ){
+        console.log( 'back from POST with:', response );
+        getMessages();
+    })
+}
+
 function onReady(){
-    console.log( 'JQ' );
+    $( '#newMessageButton' ).on( 'click', newMessage );
     getMessages();
 }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 // requires
 const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
 const app = express();
 
 // uses
 app.use( express.static( 'server/public/' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
 
 // globals
 const port = 5000;
 let messages = [];
-// test data
-messages.push( { from: 'Biz Markee', text: 'You got what I need...' } );
 
 // spin up server
 app.listen( port, ()=>{
@@ -21,3 +21,9 @@ app.get( '/messages', ( req, res )=>{
     console.log( '/messages GET' );
     res.send( messages );
 }); // end /messages GET
+
+app.post( '/messages', ( req, res )=>{
+    console.log( '/messages POST:', req.body );
+    messages.push( req.body );
+    res.sendStatus( 201 );
+})
